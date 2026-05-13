9e8271c202632c5b506457c8eacd8cf10815e656
mtxc26 <278817962+mtxc26@users.noreply.github.com>
2026-05-14 05:58:50 +0800

    fix(style): fix specific style problems

--- runtime/src/others/hash-highlight.ts
+++ runtime/src/others/hash-highlight.ts
@@ -1,19 +1,30 @@
 export async function setupHashHighlight() {
-    try {
-        const hash = window.location.hash;
-        if (!hash || hash.includes('/') || hash.includes('#:~:text=')) return;
-    
-        const el = document.getElementById(hash.slice(1));
-        if (!el) return;
-    
-        el.scrollIntoView({ block: 'center', behavior: 'smooth' });
-    
-        const range = new Range();
-        range.selectNodeContents(el);
-        const hl = new Highlight(range);
-        CSS.highlights.set('page-hash-highlight', hl);
-    }
-    catch (e) {
-        console.warn("[highlight]", "Failed to highlight the element:", e);
-    }
+    setTimeout(async () => {
+        try {
+            await new Promise(resolve => {
+                if (window.document.readyState === 'complete') {
+                    resolve();
+                } else {
+                    window.addEventListener('load', () => {
+                        resolve();
+                    }, { once: true });
+                }
+            });
+            const hash = window.location.hash;
+            if (!hash || hash.includes('/') || hash.includes('#:~:text=')) return;
+        
+            const el = document.getElementById(hash.slice(1));
+            if (!el) return;
+        
+            el.scrollIntoView({ block: 'center', behavior: 'smooth' });
+        
+            const range = new Range();
+            range.selectNodeContents(el);
+            const hl = new Highlight(range);
+            CSS.highlights.set('page-hash-highlight', hl);
+        }
+        catch (e) {
+            console.warn("[highlight]", "Failed to highlight the element:", e);
+        }
+    })
 }
