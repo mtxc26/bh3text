458b1d83ef5ede4726c099042b5ad399cf2ecf26
mtxc26 <278817962+mtxc26@users.noreply.github.com>
2026-05-14 04:14:56 +0800

    fix(ux): fix user anchor positioning

--- runtime/src/others/hash-highlight.ts
+++ runtime/src/others/hash-highlight.ts
@@ -1,12 +1,19 @@
 export async function setupHashHighlight() {
-    const hash = window.location.hash;
-    if (!hash || hash.includes('/')) return;
-
-    const el = document.getElementById(hash.slice(1));
-    if (!el) return;
-
-    const range = new Range();
-    range.selectNodeContents(el);
-    const hl = new Highlight(range);
-    CSS.highlights.set('page-hash-highlight', hl);
+    try {
+        const hash = window.location.hash;
+        if (!hash || hash.includes('/') || hash.includes('#:~:text=')) return;
+    
+        const el = document.getElementById(hash.slice(1));
+        if (!el) return;
+    
+        el.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    
+        const range = new Range();
+        range.selectNodeContents(el);
+        const hl = new Highlight(range);
+        CSS.highlights.set('page-hash-highlight', hl);
+    }
+    catch (e) {
+        console.warn("[highlight]", "Failed to highlight the element:", e);
+    }
 }
